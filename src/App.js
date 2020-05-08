79f33d4e508890a91850033848ffb4d839ce009a
nick wuerz <nawuerz@gmail.com>
2020-05-08 12:48:23 -0500

    added employee table component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from "./components/navbar";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import EmployeeTable from './components/employeeTable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <React.Fragment >
+        <NavBar />
+        <EmployeeTable />
+      </React.Fragment >
+    );
+  }
+
 }
 
 export default App;
+
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,12 +4,7 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Employee Directory</a>
-          <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"/>
-            <ul className="navbar-nav px-3">
-               <li className="nav-item text-nowrap">
-                <a className="nav-link" href="#">Search</a>
-              </li>
-            </ul>
+          <input className="form-control form-control-dark " type="text" placeholder="Filter" aria-label="Filter"/>
         </nav>
     );
 };
