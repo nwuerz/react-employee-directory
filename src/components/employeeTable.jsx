31ffc410723c25152dd4a7d6be1b949890c4cdaa
nick wuerz <nawuerz@gmail.com>
2020-05-08 15:11:00 -0500

    moved employee info to json file, changed app.js to class component

--- src/components/employeeTable.jsx
+++ src/components/employeeTable.jsx
@@ -1,122 +1,14 @@
-import React from 'react';
+import React, { Component }from 'react';
 import Employee from './employee'
+import employees from './employees.json'
 
-const employees = [
-    {
-        id: 1,
-        firstName: "Ryan",
-        lastName: "Fleming",
-        department: "Sales",
-        title: "Manager"
-    },
-    {
-        id: 2,
-        firstName: "John",
-        lastName: "Ortiz",
-        department: "Sales",
-        title: "Sales Rep"
-    },
-    {
-        id: 3,
-        firstName: "Dale",
-        lastName: "Gatlin",
-        department: "Sales",
-        title: "Sales Rep"
-    },
-    {
-        id: 4,
-        firstName: "Bronson",
-        lastName: "Skinner",
-        department: "Sales",
-        title: "Sales Rep"
-    },
-    {
-        id: 5,
-        firstName: "Eric",
-        lastName: "Ariza",
-        department: "Service",
-        title: "Manager"
-    },
-    {
-        id: 6,
-        firstName: "Teresa",
-        lastName: "Myers",
-        department: "Service",
-        title: "Service Rep"
-    },
-    {
-        id: 7,
-        firstName: "Victor",
-        lastName: "Segura",
-        department: "Service",
-        title: "Service Rep"
-    },
-    {
-        id: 8,
-        firstName: "Amanda",
-        lastName: "Mahoney",
-        department: "Service",
-        title: "Service Rep"
-    },
-    {
-        id: 9,
-        firstName: "Jason",
-        lastName: "Holland",
-        department: "Underwriting",
-        title: "Manager"
-    },
-    {
-        id: 10,
-        firstName: "Donia",
-        lastName: "Story",
-        department: "Underwriting",
-        title: "Underwriter"
-    },
-    {
-        id: 11,
-        firstName: "Joseph",
-        lastName: "Yapp",
-        department: "Underwriting",
-        title: "Underwriter"
-    },
-    {
-        id: 12,
-        firstName: "James",
-        lastName: "Crenshaw",
-        department: "Underwriting",
-        title: "Underwriter"
-    },
-    {
-        id: 13,
-        firstName: "Joseph",
-        lastName: "Loung",
-        department: "Claims",
-        title: "Manager"
-    },
-    {
-        id: 14,
-        firstName: "Weston",
-        lastName: "Britt",
-        department: "Claims",
-        title: "Claims Rep"
-    },
-    {
-        id: 15,
-        firstName: "Raul",
-        lastName: "Acosta",
-        department: "Claims",
-        title: "Claims Rep"
-    },
-    {
-        id: 16,
-        firstName: "Justin",
-        lastName: "Walters",
-        department: "Claims",
-        title: "Claims Rep"
-    },
-];
+class EmployeeTable extends Component {
 
-const EmployeeTable = () => {
+    state = {
+        employees
+    };
+        
+      render() { 
         return (
           <div className="container-fluid">
           <div className="row">
@@ -126,11 +18,11 @@ const EmployeeTable = () => {
         <table className="table table-striped table-sm">
           <thead>
             <tr>
-              <th><a href="#">Employee ID</a></th>
-              <th><a href="#">First Name</a></th>
-              <th><a href="#">Last Name</a></th>
-              <th><a href="#">Department</a></th>
-              <th><a href="#">Title</a></th>
+              <th><a>Employee ID</a></th>
+              <th><a>First Name</a></th>
+              <th><a>Last Name</a></th>
+              <th><a>Department</a></th>
+              <th><a>Title</a></th>
             </tr>
           </thead>
           <tbody>
@@ -143,6 +35,6 @@ const EmployeeTable = () => {
         </div>
         );
         }
-    
+}
  
 export default EmployeeTable;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Employee Directory</a>
+        <nav className="navbar navbar-dark bg-dark shadow">
+          <h2 className="navbar-brand">Employee Directory</h2>
           <input className="form-control form-control-dark " type="text" placeholder="Filter" aria-label="Filter"/>
         </nav>
     );
