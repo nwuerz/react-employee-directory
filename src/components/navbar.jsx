9416e26ecc51f74efb307a2d05f4f2d365bc1bd9
nick wuerz <nawuerz@gmail.com>
2020-05-08 14:44:13 -0500

    finished employee table component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <nav className="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
           <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Employee Directory</a>
           <input className="form-control form-control-dark " type="text" placeholder="Filter" aria-label="Filter"/>
         </nav>
